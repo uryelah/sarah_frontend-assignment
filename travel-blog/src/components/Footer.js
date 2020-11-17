3f2efcb20b60bdebe51eb04131344faa1ce05313
sarah <sarachamorro@gmail.com>
2020-11-17 16:53:42 -0300

    Add Footer component to travelBlog

---
+++ travel-blog/src/components/Footer.js
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">@2020 Travelize</a>
+        </li>
+        <li>
+          <a href="#">Privacy Policy</a>
+        </li>
+        <li>
+          <a href="#">Terms and conditions</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Footer;
