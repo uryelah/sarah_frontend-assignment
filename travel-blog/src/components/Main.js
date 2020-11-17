f5a90d36cb5929b0425742e3d67d4bcc258d3479
sarah <sarachamorro@gmail.com>
2020-11-17 16:56:26 -0300

    Add Main component to travelBlog

---
+++ travel-blog/src/components/Main.js
@@ -0,0 +1,20 @@
+const Main = () => {
+  return (
+    <main>
+      <header></header>
+      <section>
+        <img/>
+        <article>
+          <h1>
+            A little about me
+          </h1>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore vel odit voluptate, veritatis rem quas temporibus, dolores explicabo esse nemo. Necessitatibus totam numquam praesentium voluptates ducimus. Itaque, architecto quaerat.
+          </p>
+        </article>
+      </section>
+    </main>
+  );
+};
+
+export default Main;
