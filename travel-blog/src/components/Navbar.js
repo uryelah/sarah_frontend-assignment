60e18900f03234abc6a0c2eafaf57db57ebfdd7d
sarah <sarachamorro@gmail.com>
2020-11-17 16:51:51 -0300

    Add Navbar component to travelBlog

---
+++ travel-blog/src/components/Navbar.js
@@ -0,0 +1,25 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <div>
+            <span>Travelize</span>
+            <span>My traveling experiences</span>
+          </div>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Blog</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
